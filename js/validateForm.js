55e444ccc5437ac1301cf8893028b7ada4eae81c
xaviGH <fgargallo@gmail.com>
2019-04-19 02:32:54 +0200

    add js validation form, update register form in index.html

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,1 +1,26 @@
 
+
+function validateForm(){
+
+  var fname = document.getElementById('fname').value;
+  var surname = document.getElementById('surname').value;
+    if (fname == "") {
+      // alert("Name must be filled in");
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            //document.getElementById('er-fname').innerHTML = "Fill in your First name";
+
+
+    }
+    if (surname == "") {
+      // alert("Surname must be filled out");
+
+            event.preventDefault();
+            event.stopPropagation();
+            //document.getElementById('er-surname').innerHTML = "Fill in your Surname";
+
+
+    }
+}
